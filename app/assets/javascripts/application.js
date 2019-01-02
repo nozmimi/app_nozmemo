98167bd4d2c0791bc9d21506df54daed0fc4c8ae
nozmimi <nozmimi@yahoo.co.jp>
2019-01-02 11:49:39 +0900

    herokuにデプロイしたところjqueryが動作しなかったため、application.jsファイルを修正しました。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,11 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 // = require jquery
 // = require jquery_ujs
-// = require bootstrap
+//= require turbolinks
 // = require moment
 // = require fullcalendar
 // = require fullcalendar/lang/ja
-
+// = require bootstrap
 // = require_tree .
